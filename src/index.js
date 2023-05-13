a5ba8f5e54b28b90f6b844b6e9ec3de4d98b6fcf
j-xavier <johnnyboyxavier735@gmail.com>
2023-05-13 12:05:21 -0400

    understanding props

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// importing the react and react-dom libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// get a reference to the div with id root
+const el = document.getElementById('root');
+
+// tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// show the react component on the screen
+root.render(<App />);
